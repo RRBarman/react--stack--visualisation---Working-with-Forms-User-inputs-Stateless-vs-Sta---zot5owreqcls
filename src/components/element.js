f01ff46183289dd980c7801a5f82e1140b713eb3
RAUNAK ROY BARMAN <48653401+RRBarman@users.noreply.github.com>
2023-03-14 13:53:51 +0530

    Update element.js

--- src/components/element.js
+++ src/components/element.js
@@ -1,6 +1,13 @@
-import React from 'react';
+/*import React from 'react';
 function Element() {
   return <div className="element"></div>;
+}
+
+export default Element;*/
+import React from 'react';
+
+function Element(props) {
+  return <div className="element">{props.value}</div>;
 }
 
 export default Element;
